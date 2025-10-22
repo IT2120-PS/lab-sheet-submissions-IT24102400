78a91d6dc7aa1ef2560827c845d4a1edb0a5387a
IT24102400 <it24102400@my.sliit.lk>
2025-10-22 23:16:47 +0530

    Lab-10

---
+++ Lab Sheet 10/IT24102400.R
@@ -0,0 +1,18 @@
+getwd()
+setwd("C:\\Users\\PC\\Desktop\\3rd Semester\\PS\\Lab Sheet\\IT24102400\\Lab 10")
+getwd()
+
+
+# IT24102400
+# Amarasinghe.A.Y.U.A
+# Lab 10
+
+## Exercise
+
+
+# Part (ii)
+
+observed_counts <- c(120, 95, 85, 100)
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+chisq.test(x = observed_counts, p = probabilities)
+
